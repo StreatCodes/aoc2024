31fbdf6c822acb59a0357033813e511c60915045
Matthew Streatfield <matt.streatfield@getlocalmeasure.com>
2024-12-04 16:45:00 +1100

    Day 4 part 2

--- src/day4.zig
+++ src/day4.zig
@@ -15,51 +15,30 @@ pub fn main() !void {
     var count: u32 = 0;
     for (0..lines.items.len) |y| {
         for (0..lines.items[y].len) |x| {
-            //horizontal
-            if (x + 3 < lines.items[y].len) {
-                const matchForward = std.mem.eql(u8, lines.items[y][x .. x + 4], "XMAS");
-                if (matchForward) {
-                    std.debug.print("found horizontal {d} {d}\n", .{ x, y });
-                    count += 1;
-                }
-                const matchBackward = std.mem.eql(u8, lines.items[y][x .. x + 4], "SAMX");
-                if (matchBackward) {
-                    std.debug.print("found horizontal backwards {d} {d}\n", .{ x, y });
-                    count += 1;
-                }
-            }
-            //vertical
-            if (y + 3 < lines.items.len) {
-                if (lines.items[y][x] == 'X' and lines.items[y + 1][x] == 'M' and lines.items[y + 2][x] == 'A' and lines.items[y + 3][x] == 'S') {
-                    std.debug.print("found vertical {d} {d}\n", .{ x, y });
-                    count += 1;
-                }
-                if (lines.items[y][x] == 'S' and lines.items[y + 1][x] == 'A' and lines.items[y + 2][x] == 'M' and lines.items[y + 3][x] == 'X') {
-                    std.debug.print("found vertical backwards {d} {d}\n", .{ x, y });
-                    count += 1;
-                }
-            }
+            var match: u32 = 0;
             //diagonal \
-            if (x + 3 < lines.items[y].len and y + 3 < lines.items.len) {
-                if (lines.items[y][x] == 'X' and lines.items[y + 1][x + 1] == 'M' and lines.items[y + 2][x + 2] == 'A' and lines.items[y + 3][x + 3] == 'S') {
+            if (x + 1 < lines.items[y].len and x > 0 and y + 1 < lines.items.len and y > 0) {
+                if (lines.items[y - 1][x - 1] == 'M' and lines.items[y][x] == 'A' and lines.items[y + 1][x + 1] == 'S') {
                     std.debug.print("found diagonal \\ {d} {d}\n", .{ x, y });
-                    count += 1;
-                }
-                if (lines.items[y][x] == 'S' and lines.items[y + 1][x + 1] == 'A' and lines.items[y + 2][x + 2] == 'M' and lines.items[y + 3][x + 3] == 'X') {
+                    match += 1;
+                } else if (lines.items[y - 1][x - 1] == 'S' and lines.items[y][x] == 'A' and lines.items[y + 1][x + 1] == 'M') {
                     std.debug.print("found diagonal \\ backwards {d} {d}\n", .{ x, y });
-                    count += 1;
+                    match += 1;
                 }
             }
             //diagonal /
-            if (x >= 3 and y + 3 < lines.items.len) {
-                if (lines.items[y][x] == 'X' and lines.items[y + 1][x - 1] == 'M' and lines.items[y + 2][x - 2] == 'A' and lines.items[y + 3][x - 3] == 'S') {
+            if (x > 0 and x + 1 < lines.items[y].len and y > 0 and y + 1 < lines.items.len) {
+                if (lines.items[y - 1][x + 1] == 'M' and lines.items[y][x] == 'A' and lines.items[y + 1][x - 1] == 'S') {
                     std.debug.print("found diagonal / {d} {d}\n", .{ x, y });
-                    count += 1;
-                }
-                if (lines.items[y][x] == 'S' and lines.items[y + 1][x - 1] == 'A' and lines.items[y + 2][x - 2] == 'M' and lines.items[y + 3][x - 3] == 'X') {
+                    match += 1;
+                } else if (lines.items[y - 1][x + 1] == 'S' and lines.items[y][x] == 'A' and lines.items[y + 1][x - 1] == 'M') {
                     std.debug.print("found diagonal / backwards {d} {d}\n", .{ x, y });
-                    count += 1;
+                    match += 1;
                 }
+            }
+
+            if (match == 2) {
+                count += 1;
             }
         }
     }
