8234a94c5ff5c61f5018e92c226fa56fa0eaac00
Matthew Streatfield <matt.streatfield@getlocalmeasure.com>
2024-12-06 14:14:06 +1100

    Day 5 part 2

--- src/day5.zig
+++ src/day5.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const allocator = std.heap.page_allocator;
 
-fn isDescendant(rules: [][2]i32, first: i32, second: i32) !bool {
+fn isDescendant(rules: [][2]i32, first: i32, second: i32) bool {
     for (rules) |rule| {
         if (rule[0] == first and rule[1] == second) {
             return true;
@@ -11,14 +11,18 @@ fn isDescendant(rules: [][2]i32, first: i32, second: i32) !bool {
     return false;
 }
 
-fn correctOrder(rules: [][2]i32, first: i32, subsequent: []i32) !bool {
+fn correctOrder(rules: [][2]i32, first: i32, subsequent: []i32) bool {
     for (subsequent) |sub| {
-        if (try isDescendant(rules, first, sub)) {
+        if (isDescendant(rules, first, sub)) {
             return false;
         }
     }
 
     return true;
+}
+
+fn sort(ctx: [][2]i32, lhs: i32, rhs: i32) bool {
+    return isDescendant(ctx, lhs, rhs);
 }
 
 pub fn main() !void {
@@ -39,7 +43,7 @@ pub fn main() !void {
     }
 
     var count: i32 = 0;
-    outer: while (tokens.next()) |line| {
+    while (tokens.next()) |line| {
         var values = std.mem.splitScalar(u8, line, ',');
         var numbers = std.ArrayList(i32).init(allocator);
         defer numbers.deinit();
@@ -51,16 +55,21 @@ pub fn main() !void {
 
         std.mem.reverse(i32, numbers.items);
 
+        var correct = true;
         for (0..numbers.items.len) |i| {
-            if (!try correctOrder(rules.items, numbers.items[i], numbers.items[i + 1 ..])) {
-                continue :outer;
+            if (!correctOrder(rules.items, numbers.items[i], numbers.items[i + 1 ..])) {
+                correct = false;
             }
         }
+        if (correct) continue;
+
+        std.mem.sort(i32, numbers.items, rules.items, sort);
+
         std.mem.reverse(i32, numbers.items);
 
         const middleIndex = numbers.items.len / 2;
         const middle = numbers.items[middleIndex];
-        std.debug.print("Found correct {s}!\n", .{line});
+        std.debug.print("Corrected {s}!\n", .{line});
         count += middle;
     }
 
